69a89a0caacb5ababc6a8e71d621e018a6f30e52
TawshifTamjidAlam <tawshif27@gmail.com>
2019-04-18 23:19:44 -0700

    add method to display phrases

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,6 +7,18 @@
          
      }
 
+     addPhraseToDisplay() {
+         const phraseContainer = document.querySelector("#phrase");
+         const phraseLength = this.phrase.length;
+
+         for(let i = 0; i < phraseLength; i++){
+             const li =  document.createElement("li");
+             li.innerText = this.phrase[i];
+
+             phraseContainer.appendChild(li);
+         }
+     }
+
      set phrase (phraseValue) {
          this.phraseLowerCase = phraseValue.toLowerCase();
      }
