2de65fea3fdb18091a234cc22f1d01912e0f101a
TawshifTamjidAlam <tawshif27@gmail.com>
2019-04-22 22:40:05 -0700

    Add show matched letter method

--- js/Phrase.js
+++ js/Phrase.js
@@ -3,36 +3,47 @@
  * Project 4 - OOP Game App
  * Phrase.js */
 
- class Phrase {
-     constructor(phrase){
-         this.phrase = phrase.toLowerCase;
-         this.checkLetter = this.checkLetter.bind(this); 
-     }
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase;
+    this.checkLetter = this.checkLetter.bind(this);
+    this.showMatchedLetter = this.showMatchedLetter.bind(this);
+  }
 
-     addPhraseToDisplay() {
-         const phraseContainer = document.querySelector("#phrase");
-         const phraseLength = this.phrase.length;
+  addPhraseToDisplay() {
+    const phraseContainer = document.querySelector("#phrase");
+    const phraseLength = this.phrase.length;
 
-         for(let i = 0; i < phraseLength; i++){
-             const li =  document.createElement("li");
+    for (let i = 0; i < phraseLength; i++) {
+      const li = document.createElement("li");
 
-             li.innerText = this.phrase[i];
-             phraseContainer.appendChild(li);
+      li.innerText = this.phrase[i];
+      phraseContainer.appendChild(li);
 
-             setPhraseLiClassList(li, this.phrase[i]);
-         }
-     }
+      setPhraseLiClassList(li, this.phrase[i]);
+    }
+  }
 
-     setPhraseLiClassList(phraseLi, phraseCharacter){
-        if(/^\w+s$/.test(phraseCharacter)){
-            phraseLi.classList.add("hide", "letter", phraseCharacter);
-        } else if(/^\s+s$/.test(phraseCharacter)){
-            phraseLi.classList.add("space");
+  setPhraseLiClassList(phraseLi, phraseCharacter) {
+    if (/^\w+s$/.test(phraseCharacter)) {
+      phraseLi.classList.add("hide", "letter", phraseCharacter);
+    } else if (/^\s+s$/.test(phraseCharacter)) {
+      phraseLi.classList.add("space");
+    }
+  }
+
+  checkLetter(selectedLetter) {
+    return this.phrase.includes(selectedLetter);
+  }
+
+  showMatchedLetter(selectedLetter) {
+    if (checkLetter(selectedLetter)) {
+      Array.from(document.querySelectorAll(`.${selectedLetter}`)).map(
+        letterLi => {
+          letterLi.remove("hide");
+          letterLi.add("show");
         }
-     }
-
-     checkLetter(selectedLetter){
-         return this.phrase.includes(selectedLetter);
-     }
-     
- }
+      );
+    }
+  }
+}
