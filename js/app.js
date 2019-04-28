d5387cf1cb6aa5adba5161a1b9f90b55b48152f9
TawshifTamjidAlam <tawshif27@gmail.com>
2019-04-28 12:55:44 -0700

    add event listener for start game

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+"use strict";
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+// from https://www.phrases.com/
+ let phrases = [
+     new Phrase("run around"),
+     new Phrase("take down"),
+     new Phrase("home field advantage"),
+     new Phrase("have other fish to fry"),
+     new Phrase("pay off")
+ ];
+
+ document.addEventListener("DOMContentLoaded", () => {
+     document.querySelector("#btn__reset").addEventListener("click", () => {
+         const newGame = new Game(phrases);
+         newGame.startGame();
+     });
+ });
+
