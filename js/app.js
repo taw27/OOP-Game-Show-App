685d1222715be5287e68d1433bb4a2f8dba51b11
TawshifTamjidAlam <tawshif27@gmail.com>
2019-05-01 13:07:50 -0700

    add comments and add logic to initialise only once

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 "use strict";
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
-// from https://www.phrases.com/
+
+/* 
+    an array of phrase object to be used by the game
+ */
  let phrases = [
      new Phrase("run around"),
      new Phrase("take down"),
@@ -11,21 +11,33 @@
      new Phrase("pay off")
  ];
 
+ /* 
+    waits till the dom content has been loaded
+  */
  document.addEventListener("DOMContentLoaded", () => {
-     document.querySelector("#btn__reset").addEventListener("click", () => {
-         const newGame = new Game(phrases);
-         newGame.startGame();
-         document.querySelector("#qwerty").addEventListener("click", (event) => {
-             if(event.target.tagName === "BUTTON"){
-                 newGame.handleInteraction(event.target);
-             }
-         });
-
-         document.addEventListener("keydown", (event) => {
-             if(!event.repeat && newGame.activePhrase){
-                 newGame.handlePhysicalKeyboardInteraction(event.key);
-             }
-         })
-     });
+    let gameStarted = false;
+// iinitialises the event listeners if the game has not been started yet
+    if(!gameStarted){
+        /* 
+           sets the event listener for the game start button to create a new game object, start the game and add event listeners
+           for the onscreen and physical key buttons to handle the game interactions
+         */
+        gameStarted = true; // sets the gamestarted to true to prevent unecessary event listener assignment once the game started
+        document.querySelector("#btn__reset").addEventListener("click", () => {
+            const newGame = new Game(phrases);
+            newGame.startGame();
+            document.querySelector("#qwerty").addEventListener("click", (event) => {
+                if(event.target.tagName === "BUTTON"){
+                    newGame.handleInteraction(event.target);
+                }
+            });
+   
+            document.addEventListener("keydown", (event) => {
+                if(!event.repeat && newGame.activePhrase){
+                    newGame.handlePhysicalKeyboardInteraction(event.key);
+                }
+            })
+        });
+    }
  });
 
