d76f02fef6c3bf3f25b4efaacfcea4f80d6d1896
TawshifTamjidAlam <tawshif27@gmail.com>
2019-05-01 16:15:32 -0700

    remove game start check

--- js/app.js
+++ js/app.js
@@ -15,19 +15,15 @@
     waits till the dom content has been loaded
   */
  document.addEventListener("DOMContentLoaded", () => {
-    let gameStarted = false;
-// iinitialises the event listeners if the game has not been started yet
-    if(!gameStarted){
         /* 
            sets the event listener for the game start button to create a new game object, start the game and add event listeners
-           for the onscreen and physical key buttons to handle the game interactions
+           for the onscreen and physical key buttons to handle the game
          */
-        gameStarted = true; // sets the gamestarted to true to prevent unecessary event listener assignment once the game started
         document.querySelector("#btn__reset").addEventListener("click", () => {
             const newGame = new Game(phrases);
             newGame.startGame();
             document.querySelector("#qwerty").addEventListener("click", (event) => {
-                if(event.target.tagName === "BUTTON"){
+                if(event.target.tagName === "BUTTON" && newGame.activePhrase){ // checks if activePhrase is not null to skip previous listeners
                     newGame.handleInteraction(event.target);
                 }
             });
@@ -38,6 +34,8 @@
                 }
             })
         });
-    }
+    
  });
+
+ 
 
