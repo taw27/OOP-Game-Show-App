63a5aaab5765fadb0c42c68b0f2252c0fc736d1e
TawshifTamjidAlam <tawshif27@gmail.com>
2019-05-01 13:07:58 -0700

    add comments

--- js/Game.js
+++ js/Game.js
@@ -1,9 +1,12 @@
 "use strict";
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
-
+/*
+    Game class that handles all the logic of the game
+ */
  class Game {
+     /* 
+        Constructor initialises a game object setting missed propert to 0, the phrases property to array of phrases
+        passed in as argument and the activePhrase property to null. Binds this to methods which require it
+      */
      constructor(phrases){
          this.missed  = 0;
          this.phrases = phrases;
@@ -12,10 +15,19 @@
          this.resetForGameOver = this.resetForGameOver.bind(this); // bind this object so that wheen resetForGameOver is called in setTimeOut it behaves as expected
      }
 
+     /* 
+        returns true if all the letters have been revealed and false if not by selecting by the .hide class
+      */
      checkForWin(){
         return document.querySelectorAll('#phrase li.hide').length === 0
     }
 
+    /* 
+        takes in a qwerty button element of the screen. Disables the button element, if the button matches a letter
+        in the active phrase, reveals the letter and adds chosen class the button. If the game is won the gsmeOver method.
+        If the button does not match any letter in  the phrase, then adds the wrong class to the button and calls the removeLife
+        method
+     */
      handleInteraction(buttonElement){
         buttonElement.disabled = true;
          if(this.activePhrase.checkLetter(buttonElement.innerText)){
@@ -30,13 +42,17 @@
          }
      }
 
+     /* 
+        Takes in the key character pressed, if its a letter in the english alphabet then retreives the corresponding onscreen button element
+        and calls the the handleInteraction method with the button element passed in if it is not disabled
+      */
      handlePhysicalKeyboardInteraction(key){
             if(/[a-zA-Z]/i.test(key)){
                 const correspondingButton = Array.from(document.querySelectorAll(`.key`)).filter(
                 keyButton => {
                     return keyButton.innerText.includes(key);
                 }
-                )[0];
+                )[0]; // gets the corresponding onscreen button element
 
                 if(!correspondingButton.disabled){
                     this.handleInteraction(correspondingButton);
@@ -44,6 +60,10 @@
             } 
      }
 
+     /* 
+        displays the overlay with a win message if game won or a lose message with game loss.
+        Resets the game at the end by calling the resetForGameOver message
+      */
      gameOver(){
          const overlay = document.querySelector('#overlay');
          const checkWin = this.checkForWin();
@@ -52,15 +72,21 @@
          overlay.classList.remove("start", "win", "lose");
          checkWin ? overlay.classList.add("win") : overlay.classList.add("lose");
 
-         window.setTimeout(this.resetForGameOver, 800);
+         window.setTimeout(this.resetForGameOver, 800); // so that the game is reset after the overlay message transition finishes
      }
 
+    /* 
+     returns a random phrase object from the phrases array
+     */
      getRandomPhrase(){
          const randomIndex = Math.floor(Math.random() * Math.floor(this.phrases.length - 1));
          return this.phrases[randomIndex];
      }
 
-     resetKeyBoard(){
+  /* 
+     Resets the keyboard by removing any wrong and/or chosen css classes from the onscreen qwerty buttons and enables all off them  
+   */ 
+    resetKeyBoard(){
         Array.from(document.querySelectorAll(`.keyrow button`)).map(
             button => {
               button.classList.remove("wrong", "chosen");
@@ -69,6 +95,10 @@
           );
      }
 
+     /* 
+      Replaces a live heart image with lose heart image to simulate lost life. Calls the gameOver 
+      method if all lives run out
+      */
      removeLife(){
          const hearts = document.querySelectorAll('li.tries img');
          const numHearts =  hearts.length;
@@ -83,6 +113,9 @@
          }
      }
 
+     /* 
+        removes all the li descandent of the #phrase container
+      */
      removePhraseLi(){
         Array.from(document.querySelectorAll(`#phrase li`)).map(
             letterLi => {
@@ -91,6 +124,9 @@
           );
         }
 
+    /*
+        replaces all heart image src to live heart image to reset the lives
+    */
      resetHearts() {
         Array.from(document.querySelectorAll(`li.tries img`)).map(
              heartImg => {
@@ -99,6 +135,10 @@
           );
      }
 
+     /* 
+        calls the appropriate methods to reset the game, with the phrase removed, hearts back to live hearts,
+        on screen keyboard enabled and normal, missed property to 0 and activePhrase property to null
+      */
      resetForGameOver(){
         this.removePhraseLi();
         this.resetHearts();
@@ -107,6 +147,9 @@
         this.activePhrase = null;
      }
 
+     /* 
+        Starts the game by hiding the overlay, setting the active phrase object and adding it to the display
+      */
      startGame(){
         document.querySelector('#overlay').classList.add('overlay-hide');
          this.activePhrase = this.getRandomPhrase();
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,15 +1,22 @@
 "use strict";
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
 
+/* 
+  Phrase class to represent a phrase and with methods to interact with the phrase
+ */
 class Phrase {
+/* 
+    constructor to initialise a phrase object, sets the phrase property to the phrase argument passed in lower case
+    and makes necessary method bindings 
+ */
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
     this.checkLetter = this.checkLetter.bind(this);
     this.showMatchedLetter = this.showMatchedLetter.bind(this);
   }
 
+  /* 
+    creates li elements for the phase characters with appropriate classes and adds them to the display
+   */
   addPhraseToDisplay() {
     const phraseContainer = document.querySelector("#phrase");
     const phraseLength = this.phrase.length;
@@ -24,6 +31,10 @@ class Phrase {
     }
   }
 
+  /* 
+    takes in a phrase character li element, the phrase character. Sets the class to letter and hide if it is a letter character
+    and space if it is a space character
+   */
   setPhraseLiClassList(phraseLi, phraseCharacter) {
     if (/^[a-zA-Z]$/i.test(phraseCharacter)) {
       phraseLi.classList.add("hide","letter", phraseCharacter);
@@ -32,10 +43,16 @@ class Phrase {
     }
   }
 
+  /* 
+    rerturns true if the phrase includes the selected letter argument passed in and returns false if not
+   */
   checkLetter(selectedLetter) {
     return this.phrase.includes(selectedLetter);
   }
 
+  /* 
+    if a phrase includes the passed in character argument, then it displays it by adding the show class and removing hide class
+   */
   showMatchedLetter(selectedLetter) {
     if (this.checkLetter(selectedLetter)) {
       Array.from(document.querySelectorAll(`.${selectedLetter}`)).map(
