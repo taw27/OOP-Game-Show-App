a84988fd90b98b2e491e3100bfa4348f5ae5219c
TawshifTamjidAlam <tawshif27@gmail.com>
2019-04-30 21:22:44 -0700

    change regex from word character to only letters

--- js/Phrase.js
+++ js/Phrase.js
@@ -25,7 +25,7 @@ class Phrase {
   }
 
   setPhraseLiClassList(phraseLi, phraseCharacter) {
-    if (/^\w$/i.test(phraseCharacter)) {
+    if (/^[a-z A-Z]$/i.test(phraseCharacter)) {
       phraseLi.classList.add("hide","letter", phraseCharacter);
     } else if (/^[ ]$/.test(phraseCharacter)) {
       phraseLi.classList.add("space");
